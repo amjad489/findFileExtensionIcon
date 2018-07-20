305f45975fb7e7cb99f9bfa894af42819453e57d
amjad <amjad.hussain@tajawal.com>
2018-07-21 01:48:32 +0400

    added space after the span element

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 module.exports = function(filename) {
     let fileExt = filename.split('.').pop();
     if ((/(txt|out|log)$/i).test(fileExt)){
-        return "<i class=\"far fa-file-alt\"></i>";
+        return "<i class=\"far fa-file-alt\"></i> ";
     }
     else if ((/(gz|zip|bz2|7z|tar|nar|jar|war|ear)$/i).test(fileExt)){
-        return "<i class=\"fas fa-file-archive\"></i>";
+        return "<i class=\"fas fa-file-archive\"></i> ";
     }
     else if ((/(json|xml|py|pyc|javac|java|pl|asp|js|html|yml|yaml)$/i).test(fileExt)){
-        return "<i class=\"far fa-file-code\"></i>";
+        return "<i class=\"far fa-file-code\"></i> ";
     }
     else if ((/(pdf)$/i).test(fileExt)){
-        return "<i class=\"far fa-file-pdf\"></i>";
+        return "<i class=\"far fa-file-pdf\"></i> ";
     }
     else if ((/(xls|xlsx)$/i).test(fileExt)){
-        return "<i class=\"fas fa-file-excel\"></i>";
+        return "<i class=\"fas fa-file-excel\"></i> ";
     }
     else if ((/(doc|docx)$/i).test(fileExt)){
-        return "<i class=\"far fa-file-word\"></i>";
+        return "<i class=\"far fa-file-word\"></i> ";
     }
     else if ((/(ppt|pptx)$/i).test(fileExt)){
-        return "<i class=\"far fa-file-powerpoint\"></i>";
+        return "<i class=\"far fa-file-powerpoint\"></i> ";
     }
     else if ((/(gif|jpg|jpeg|tiff|png)$/i).test(fileExt)){
-        return "<i class=\"far fa-file-image\"></i>";
+        return "<i class=\"far fa-file-image\"></i> ";
     }
     else if ((/(avi|wmv|flv|mpg|mp4|divx)$/i).test(fileExt)){
-        return "<i class=\"far fa-file-video\"></i>";
+        return "<i class=\"far fa-file-video\"></i> ";
     }
     else if ((/(wav|mp3)$/i).test(fileExt)){
-        return "<i class=\"far fa-file-audio\"></i>";
+        return "<i class=\"far fa-file-audio\"></i> ";
     }
     else {
-        return "<i class=\"fas fa-file\"></i>";
+        return "<i class=\"fas fa-file\"></i> ";
     }
 };
